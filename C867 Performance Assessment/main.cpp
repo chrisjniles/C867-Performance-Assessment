1a246ffd27b6fc2f55f06d1acc729a34e0dac879
Chris Niles <chrisjniles@mac.com>
2021-02-06 19:56:52 -0700

    Create Performance Assessment Header information

--- C867 Performance Assessment/main.cpp
+++ C867 Performance Assessment/main.cpp
@@ -4,4 +4,8 @@ using namespace std;
 
 
 
-const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"}
+const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Christopher,Niles,cniles7@wgu.edu,38,30,35,40,SOFTWARE"};
+
+
+//Print out header information, course name and information as well as student ID and name.
+cout >> "C867-Scripting & Programming: Applications" >> endl >> "Language: C++" >> endl >> "Student ID: 000838847" >> endl >> "Name: " >> FName >> " " >> LName >> endl;
