d9135fd2cda0971b5eaf071f5f60298f31323ec3
Chris Niles <chrisjniles@mac.com>
2021-02-06 20:06:01 -0700

    forget to create inside of main(), durrr

--- C867 Performance Assessment/main.cpp
+++ C867 Performance Assessment/main.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
 #include <string>
 using namespace std;
 
+int main() {
+
+	const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Christopher,Niles,cniles7@wgu.edu,38,30,35,40,SOFTWARE" };
 
 
+	//Print out header information, course name and information as well as student ID and name.
+	cout << "C867-Scripting & Programming: Applications" << endl << "Language: C++" << endl << "Student ID: 000838847" << endl << "Name: " << FName << " " << LName << endl;
 
-const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Christopher,Niles,cniles7@wgu.edu,38,30,35,40,SOFTWARE"};
+	return 0;
 
-
-//Print out header information, course name and information as well as student ID and name.
-cout << "C867-Scripting & Programming: Applications" << endl << "Language: C++" << endl << "Student ID: 000838847" << endl << "Name: " << FName << " " << LName << endl;
+}
