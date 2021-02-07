9eceea119e6b58f3d2591fb2afebe124b380d0a2
Chris Niles <chrisjniles@mac.com>
2021-02-06 19:48:32 -0700

    insert studentData table into main

---
+++ C867 Performance Assessment/main.cpp
@@ -0,0 +1,7 @@
+#include <string>
+using namespace std;
+
+
+
+
+const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"}
