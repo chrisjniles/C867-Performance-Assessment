358579299b4d672f10b1dd6e4bf3e77ae5920193
Chris Niles <chrisjniles@mac.com>
2021-02-09 19:47:34 -0700

    a

--- C867 Performance Assessment/main.cpp
+++ C867 Performance Assessment/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
 
 	//Print out header information, course name and information as well as student ID and name.
-	cout << "C867-Scripting & Programming: Applications" << endl << "Language: C++" << endl << "Student ID: 000838847" << endl << "Name: " << FName << " " << LName << endl;
+	cout << "C867-Scripting & Programming: Applications" << endl << "Language: C++" << endl << "Student ID: 000838847" << endl << "Name: Christopher Niles" << endl;
 
 	return 0;
 
