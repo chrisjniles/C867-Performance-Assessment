650402e4bc39a44ecb308d66c2e3110f6a106087
Chris Niles <chrisjniles@mac.com>
2021-02-13 11:34:36 -0700

    Building Student class

--- C867 Performance Assessment/student.h
+++ C867 Performance Assessment/student.h
@@ -8,6 +8,28 @@ using namespace std;
 
 // Section D1 - Create the class Student
 
+class Student {
+public:
+	Student();
+	Student(
+		string studentId,
+		string firstName,
+		string lastName,
+		string emailAddress,
+		string age,
+		int daysInCourse[],
+		Degree type);
+	~Student();
+	// Section D2a - accessors
+	string GetStudentID();
+	string GetFirstName();
+	string GetLastName();
+	string GetEmailAddress();
+	string GetAge();
+	int* GetDaysInCourse();
 
+private:
+
+};
 
 #endif
