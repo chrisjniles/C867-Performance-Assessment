ffd7210fb7be571ca773844bdc3ad746a9100bec
Chris Niles <chrisjniles@mac.com>
2021-02-19 11:30:43 -0700

    Formatting header information

--- C867 Performance Assessment/main.cpp
+++ C867 Performance Assessment/main.cpp
@@ -10,9 +10,9 @@ int main() {
 
 	//Print out header information; course name and information as well as student ID and name.
 	cout << "C867-Scripting & Programming: Applications" << endl
-		<< "Language: C++" << endl
-		<< "Student ID: 000838847" << endl
-		<< "Name: Christopher Niles" << endl;
+		<< "Language:	C++" << endl
+		<< "Student ID:	000838847" << endl
+		<< "Name:		Christopher Niles" << endl;
 
 	Roster* classRoster = new Roster(maxSize);   //create classRoster, add each student to classRoster
 
