3d8da071fb0bb950e111ee0e6130b00aad22636a
Chris Niles <chrisjniles@mac.com>
2021-03-27 18:43:30 -0600

    corrected enum type

--- C867 Performance Assessment/degree.h
+++ C867 Performance Assessment/degree.h
@@ -1,11 +1,11 @@
 #ifndef DEGREE_H
 #define DEGREE_H
-
 #include <string>
+using namespace std;
 
 //Section C - Define enum data type DegreeProgram
 
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
-string degreeProgramType[] = {"SECURITY","NETWORK","SOFTWARE"}
+enum class DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+string degreeProgramType[] = { "SECURITY","NETWORK","SOFTWARE" };
 
 #endif
